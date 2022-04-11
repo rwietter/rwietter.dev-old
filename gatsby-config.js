3c78eb762e610905c2962bd62ff5338be099048e
rwietter <mauriciobw17@gmail.com>
2022-04-11 16:04:03 -0300

    feat: analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -125,11 +125,11 @@ module.exports = {
         // Setting this parameter is optional
         anonymize: true,
         // Setting this parameter is also optional
-        respectDNT: true,
+        // respectDNT: true,
         // Avoids sending pageview hits from custom paths
-        exclude: [],
+        // exclude: [],
         // Delays sending pageview hits on route update (in milliseconds)
-        pageTransitionDelay: 0,
+        // pageTransitionDelay: 0,
         // Enables Google Optimize using your container Id
         // optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID",
         // Enables Google Optimize Experiment ID
@@ -137,13 +137,13 @@ module.exports = {
         // Set Variation ID. 0 for original 1,2,3....
         // variationId: "YOUR_GOOGLE_OPTIMIZE_VARIATION_ID",
         // Defers execution of google analytics script after page load
-        defer: false,
+        // defer: false,
         // Any additional optional fields
-        sampleRate: 5,
-        siteSpeedSampleRate: 10,
-        cookieDomain: "rwietter.xyz",
+        // sampleRate: 5,
+        // siteSpeedSampleRate: 10,
+        // cookieDomain: "rwietter.xyz",
         // defaults to false
-        enableWebVitalsTracking: true,
+        // enableWebVitalsTracking: true,
       },
     },
   ],
